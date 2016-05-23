ffc07d64914efe4cb7052ab1a4ee1e98d4e2cc81
JurgenVerdievel <j_verdievel@yahoo.com>
2016-05-23 21:43:23 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -77,9 +77,7 @@
 						datastreamIds += datastream.id + " ";
 					});
 				}
-				feedData.datastreams.sort();  //test
-				//feedData.datastreams.(function(datastream) {
-				feedData.datastreams.forEach(function(datastream) {
+			//	feedData.datastreams.forEach(function(datastream) {
 					var now = new Date();
 					var then = new Date();
 					var updated = new Date;
@@ -206,7 +204,7 @@
 					} else {
 						$('#feed-' + feedId + ' .datastreams .datastream-' + datastream.id + ' .graphWrapper').html('<div class="alert alert-box no-info">Sorry, this datastream does not have any associated data.</div>');
 					}
-				});
+			//	});
 			}
 			$('#loadingData').foundation('reveal', 'close');
 		});
