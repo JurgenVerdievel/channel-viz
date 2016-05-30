77304fe9310ed0155fb7f5d652386eac75374211
JurgenVerdievel <j_verdievel@yahoo.com>
2016-05-30 15:08:50 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,7 +12,7 @@
 */	
 
 	var defaultKey		= 'AkDCUKiOhUnUZWDKBhQ7DYuf0MHlmmzK9QCGfenFw0WCFC7B', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [396478290!Scale1], // Comma separated array of Xively Feed ID numbers
+		defaultFeeds	= ['396478290!Scale1'], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'iBees', // Replaces Xively logo in the header
 		dataDuration	= '1week', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
