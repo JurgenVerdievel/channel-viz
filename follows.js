5900caa95e91e20175ecd5ce7b0e61bab04d3f82
JurgenVerdievel <j_verdievel@yahoo.com>
2016-05-30 11:27:57 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -154,12 +154,12 @@
 										min: minValue,
 										//min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										//max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
-										padding: {
-											top: 0.02,
-											right: 0.02,
-											bottom: 0.02,
-											left: 0.02
-										},
+										//padding: {
+										//	top: 0.02,
+										//	right: 0.02,
+										//	bottom: 0.02,
+										//	left: 0.02
+										//},
 										series: series
 									});
 
