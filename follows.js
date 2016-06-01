1c25e4e990dc41432143c19f77421de2d666c0a4
JurgenVerdievel <j_verdievel@yahoo.com>
2016-06-01 20:37:07 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -76,7 +76,8 @@
 						datastreamIds += datastream.id + " ";
 					});
 				}
-				feedData.datastreams.forEach(function(datastream) {
+//				feedData.datastreams.forEach(function(datastream) {
+				feedData.datastreams.function(datastream) {
 					var now = new Date();
 					var then = new Date();
 					var updated = new Date;
@@ -213,7 +214,7 @@
 					} else {
 						$('#feed-' + feedId + ' .datastreams .datastream-' + datastream.id + ' .graphWrapper').html('<div class="alert alert-box no-info">Sorry, this datastream does not have any associated data.</div>');
 					}
-				});
+				};
 			}
 			$('#loadingData').foundation('reveal', 'close');
 		});
