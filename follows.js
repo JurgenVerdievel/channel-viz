94c522bd7fe8ef33fb0d496dd361545d6b66838f
JurgenVerdievel <j_verdievel@yahoo.com>
2016-06-08 17:40:08 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -231,9 +231,6 @@
 				thisFeedId = id;
 			}
 			id = thisFeedId;
-//                        id = 'Scale1';
-//			thisFeedid = defaultFeeds[0];
-//			thisFeedDataStreams = 'Scale1';
 			if($('#feed-' + id)) {
 				$('#feed-' + id).remove();
 			}
