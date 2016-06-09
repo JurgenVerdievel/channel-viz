92deae2091964ea3688e31d24832fa0c070fafac
JurgenVerdievel <j_verdievel@yahoo.com>
2016-06-09 21:39:59 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -91,7 +91,7 @@
 					then.setTime(now.getTime() - diff);          //eg date of 1 week ago
 					if(updated.getTime() > then.getTime()) {         //last updated data less than 1 week ago
 						if(datastreamIds && datastreamIds != '' && datastreamIds.indexOf(datastream.id) >= 0) {      //correct datastream identified
-							xively.datastream.history(feedId, datastream.id, {duration: duration, interval: interval, limit: 1000}, function(datastreamData) {       //puts data in datastreamData
+							xively.datastream.history(feedId, datastream.id, {duration: '2weeks', interval: '1800', limit: 1000}, function(datastreamData) {       //puts data in datastreamData
 
 								var series = [];
 								var points = [];
