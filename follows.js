b09b20461c0d1c8bb467205021bf97d80a980030
JurgenVerdievel <j_verdievel@yahoo.com>
2016-06-13 11:14:23 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -11,13 +11,13 @@
 		hideForm		= 0;
 */	
 
-	var defaultKey		= 'XEiYL8YOsGud2mVF0tMOe6l4EqrARiyKKSoaFqxYpMjW7HP2', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [''], // Comma separated array of Xively Feed ID numbers 396478290!Scale1
+	var defaultKey		= 'oAFLQCo3Sq24RAureB0XTfdHtGY1lxKDM8oV90urYuZIF77L', // Unique master Xively API key to be used as a default
+		defaultFeeds	= ['1961178354!S1'], // Comma separated array of Xively Feed ID numbers 396478290!Scale1
 		applicationName	= 'iBees', // Replaces Xively logo in the header
 		dataDuration	= '1week', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 1000, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
